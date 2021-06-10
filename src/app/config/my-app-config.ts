e500994a531044eb5b80182372e21a47cf6d478a
Wotan034 <john.gibney034@gmail.com>
2021-06-10 14:52:07 -0400

    final Commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3gqubrYp6mN76Y5d6',
+        issuer: 'https://dev-2134459.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
